8cb1f6f458eaea7d4c411ca80d0ec3045c94e892
Sarthak <sarthak.siddhu.das@gmail.com>
2023-05-14 16:24:13 +0530

    edited contexts

--- client/src/contexts/index.tsx
+++ client/src/contexts/index.tsx
@@ -24,7 +24,7 @@ export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
     "(prefers-color-scheme: dark)"
   ).matches;
 
-  const systemPreference = isSystemPreferenceDark ? "dark" : "light";
+  const systemPreference = isSystemPreferenceDark ? "light" : "light";
   const [mode, setMode] = useState(
     colorModeFromLocalStorage || systemPreference
   );
@@ -35,7 +35,7 @@ export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
 
   const setColorMode = () => {
     if (mode === "light") {
-      setMode("dark");
+      setMode("light");
     } else {
       setMode("light");
     }
@@ -48,7 +48,7 @@ export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
         mode,
       }}
     >
-      <ThemeProvider theme={mode === "light" ? LightTheme : DarkTheme}>
+      <ThemeProvider theme={mode === "light" ? LightTheme : LightTheme}>
         {children}
       </ThemeProvider>
     </ColorModeContext.Provider>
